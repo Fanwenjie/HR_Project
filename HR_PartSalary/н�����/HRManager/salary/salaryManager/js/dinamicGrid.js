567835eeb66e196532936b89416e012aa55bd299
Fanwenjie <1321907687@qq.com>
2011-12-19 22:50:54 +0800

    修改了动态表上的合计功能

--- HR_PartSalary/н�����/HRManager/salary/salaryManager/js/dinamicGrid.js
+++ HR_PartSalary/н�����/HRManager/salary/salaryManager/js/dinamicGrid.js
@@ -4,7 +4,7 @@
 ** 作者： fanwenjie
 ** 变更时间： 2011-12-18
 ** 作用：动态表头函数 url为grid加载路径，head为表头字符串，以“,”分割开，注意最后也要加个“,”，tbar1为grid的头部工具栏
-         tbar2为双行工具栏
+         tbar2为双行工具栏，预留1、2行为工号和姓名之用
 **********************************************************************************************************************/  
 function createGrid(url,head,tbar1,tbar2)
 {
@@ -23,14 +23,37 @@ function createGrid(url,head,tbar1,tbar2)
 /**********************************************************************************************************************
 ** 作者： fanwenjie
 ** 变更时间： 2011-12-18
+** 作用：汇总函数
+**********************************************************************************************************************/ 
+    function summery(store,record,option)
+    {                
+        var sumRecord='{';
+        store.fields.each(function(item,index){//取其值域
+            var name=item.name;
+            var value='null';
+            if(index==0) value='"<strong>合计：<\strong>"';
+            if(index!=0 && index!=1) value='"<strong>'+store.sum(name).toString()+'<\strong>"';
+            sumRecord+=name+':'+value+',';
+        });
+        sumRecord = sumRecord.substring(0, sumRecord.length - 1)+'}';
+        var tt=new store.recordType(new Ext.util.JSON.decode(sumRecord));
+        store.add(tt);
+    }
+/**********************************************************************************************************************
+** 作者： fanwenjie
+** 变更时间： 2011-12-18
 ** 作用：定义grid
-**********************************************************************************************************************/  
+**********************************************************************************************************************/ 
+     
     var cm = new Ext.grid.ColumnModel({
         columns: new Ext.util.JSON.decode(headA)
     });
     //store对象
     var ds = new Ext.data.JsonStore({
         data: [[1,2,3,4],[2,3,4,5]],
+        listeners:{
+			    'load':summery
+			},
 //        url:url,
 //        root:'data',//将来要添加上的代码
 //		totalProperty:'total',
